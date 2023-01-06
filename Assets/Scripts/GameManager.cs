cfe65ef571ebab7e2e638d1059eac358ddc22fb5
rg81893 <rguillorit@gmail.com>
2023-01-06 23:12:35 +0100

    Remove 1st object if sup 10

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,19 +7,20 @@ using UnityEngine.Rendering;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] int distanceZ;
-    [SerializeField] GameObject cube;
+    [SerializeField] GameObject obj;
     [SerializeField] int maxObjects;
+    List<GameObject> gameObjects;
     bool created;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObjects = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && maxObjects < 10)
+        if (Input.GetMouseButtonDown(0) && gameObjects.Count < maxObjects)
         {
             Vector3 mousePosition = Input.mousePosition;
             mousePosition.z = distanceZ;
@@ -29,8 +30,16 @@ public class GameManager : MonoBehaviour
 
     bool SpawnCube(Vector2 mousePosition)
     {
-        Instantiate(cube, mousePosition, Quaternion.identity);
-        maxObjects++;
+        GameObject objectNew = Instantiate(obj, mousePosition, Quaternion.identity) as GameObject;
+        
+        gameObjects.Add(objectNew);
+        if (gameObjects.Count == maxObjects)
+        {
+            GameObject objToDestroy = gameObjects[0];
+            Destroy(objToDestroy);
+            gameObjects.RemoveAt(0);
+        }
+       
         return created = true;
     }
 }
